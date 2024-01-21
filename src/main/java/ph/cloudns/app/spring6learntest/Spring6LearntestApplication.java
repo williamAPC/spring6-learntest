42be5f349efdd24fe94d48bd46def439dac6a8e9
williamAPC <w.h.b.a.p.c@gmail.com>
2024-01-21 00:26:35 +0000

    commitFirst

---
+++ src/main/java/ph/cloudns/app/spring6learntest/Spring6LearntestApplication.java
@@ -0,0 +1,13 @@
+package ph.cloudns.app.spring6learntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6LearntestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6LearntestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ph/cloudns/app/spring6learntest/Spring6LearntestApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.cloudns.app.spring6learntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6LearntestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
